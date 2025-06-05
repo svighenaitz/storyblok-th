598cc002ded06d420d712bc8ea34683809dff685
Giuliano De Ioanni <giulianodeioannigcp@gmail.com>
2025-06-05 17:48:22 +0200

    config: add allowed hosts for development server in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -30,5 +30,8 @@ export default defineConfig({
         }
       }
     }
+  },
+  server: {
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'devserver-develop--storyblok-th.netlify.app']
   }
 });
